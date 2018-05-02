720cde9c075aa37c273a805c0a82d93043714577
andrewayoub <andrew.a.mikhaeel@gmail.com>
2018-05-02 17:35:51 +0200

    convert repo to jumpscale module and add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from setuptools.command.install import install as _install
+import os 
+def _post_install(libname, libpath):
+    from js9 import j
+    from IPython import embed; embed()
+    # add this plugin to the config
+    c = j.core.state.configGet('plugins', defval={})
+    c[libname] = "%s/github/zero-os/integration-scripts/JumpScale9Benchmark" % j.dirs.CODEDIR
+    # c[libname] = libpath
+    j.core.state.configSet('plugins', c)
+    j.sal.process.execute(
+        "pip3 install 'git+https://github.com/trezor/python-mnemonic.git'")
+    j.tools.jsloader.generate()
+
+
+class install(_install):
+
+    def run(self):
+        _install.run(self)
+        libname = self.config_vars['dist_name']
+        libpath = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), libname)
+        self.execute(_post_install, (libname, libpath),
+                     msg="Running post install task")
+
+setup(
+    name='JumpScale9Benchmark',
+    version='1.0.0',
+    description='Benchmark tool for zeroos',
+    url='https://github.com/zero-os/integration-scripts',
+    author='GreenItGlobe',
+    author_email='info@gig.tech',
+    license='Apache',
+    
+    cmdclass={
+        'install': install,
+    },
+)
